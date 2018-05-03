69d620c19b1852c6afe2f16573553897e5e30e61
SIMON HE <simon.he@cargosamrt.com>
2018-05-03 17:44:24 +0800

    add common components

---
+++ client/components/common/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='header'>
+      <div>
+        <div class="menu-item">
+          Home
+        </div>
+        <div class="menu-item">|</div>
+        <div class="menu-item">Menu</div>
+        <div class="menu-item signin">Sign In</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ client/components/common/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import footer from './Footer';
+import header from './Header';
+export default {
+  components : {
+    'footer',
+    'header'
+  },
+  data () {
+    return {
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
+
