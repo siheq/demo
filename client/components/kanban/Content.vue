242c8479369bf0eaca4cc1d854f73c5d68af4593
simon <2711032242@qq.com>
2018-08-15 22:00:45 +0800

    update kanban

--- client/components/kanban/Content.vue
+++ client/components/kanban/Content.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <el-col :span='8'>
-      <div class='mission-panel'>
-        <div class='mission-title'>Mission</div>
-        <div v-for='item in cardData'>
+    <el-row :gutter='20'>
+      <el-col :offset='1' :span='8'>
+        <div class='mission-panel'>
+          <div class='mission-title'>Mission</div>
+          <div class='mission-item' v-for='(item, index) in cardData'>
+            <div @dblclick='clickCard(index)'>
+              <bubble-card>
+                <div v-if='activeIndex === index'>
+                  <el-input
+                    style='padding-top: 10px'
+                    type='textarea'
+                    :rows='2'
+                    placeholder='请输入内容'
+                    v-model='cardData[index].content'>
+                  </el-input>
+                  <el-button type='primary' size='mini' @click='saveCard'>Finish</el-button>
+                </div>
+                <div v-else>
+                  {{item.content}}
+                </div>
+              </bubble-card>
+            </div>
+          </div>
           <div>
-            <bubble-card>
-              {{item}}
-            </bubble-card>
+            <a @click='addMission' href='javascript:void(0)'>Add Card</a>
           </div>
         </div>
-      </div>
-    </el-col>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -22,9 +39,36 @@ export default {
   components: {
     BubbleCard,
   },
-  data: () => ({
-    cardData: [1, 2],
-  }),
+  data () {
+    return {
+      cardData: [],
+      activeIndex: -1,
+    };
+  },
+  methods: {
+    addMission () {
+      this.cardData.push({
+        content: '',
+      });
+    },
+    clickCard (index) {
+      if (this.activeIndex !== index) {
+        // console.log('click card');
+        this.activeIndex = index;
+      }
+      // if (this.activeIndex === index) {
+      //   this.activeIndex = -1;
+      // }
+      // else {
+      //   this.activeIndex = index;
+      // }
+    },
+    saveCard () {
+      // To Do call api to save data
+      console.log('save card');
+      this.activeIndex = -1;
+    },
+  },
 };
 </script>
 
@@ -38,6 +82,9 @@ export default {
     text-align: center;
     font-size: 20px;
     font-weight: 700;
+  }
+  .mission-item {
+    margin: 10px 0;
   }
 }
 </style>
